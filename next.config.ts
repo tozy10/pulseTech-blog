f68b4ea1907af1d700ea76ca2f7fa98fa6f5d579
Ernest Tsvangirai <ernesttozy@gmail.com>
2025-08-17 21:40:31 +0200

    edit image domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains:['placehold.co', 'www.freepik.com']
+    domains:['placehold.co', 'www.freepik.com'],
+    
+    remotePatterns: [
+      new URL('https://psjoersjmxrgsioticxz.supabase.co/'),
+      new URL('https://placehold.co/')
+    ],
+    
   }
 };
 
